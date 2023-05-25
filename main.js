a3e56183a19d3b153f368fab1f59cb3a7fc3de50
siixty <melosbobi11@gmail.com>
2023-05-25 15:58:31 +0200

    +

---
+++ main.js
@@ -0,0 +1,45 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 160, density: { enable: true, value_area: 800 } },
+    color: { value: "#00bdd4" },
+    opacity: {
+      value: 0.4,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.2,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 },
+    },
+  },
+  retina_detect: true,
+});
+
+
+let cont = document.querySelector('.container');
+let layers = document.querySelector('.hero');
+
+
+cont.onscroll = function () {
+  let X = cont.scrollTop;
+
+  layers.style.left = X/12 + 'px';
+}
